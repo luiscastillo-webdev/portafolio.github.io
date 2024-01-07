903cdfdf0f993e1aec1000fa77a282887c290511
luiscastillo-webdev <cncompusistema@gmail.com>
2024-01-07 15:19:06 -0400

    fijar api insegura

--- src/components/Clima.jsx
+++ src/components/Clima.jsx
@@ -14,7 +14,7 @@ const Clima = () => {
            
             const city = 'valencia';
             const country = 'VE';
-            const url = `http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${apiId}`;
+            const url = `https://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${apiId}`;
         
            const response = await fetch(url);
            const data = await response.json();  
